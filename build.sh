f6d5aff9c0a372cf3a7a7c3d2558edad838e9207
johnathan <johnathanfercher22@gmail.com>
2018-06-17 12:37:56 -0300

    first commit

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+cargo update
 cargo build
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate vss_core_rust;
+
+use std::{thread, time};
+use self::vss_core_rust::communications::command_sender::CommandSender;
+use vss_core_rust::domain::team_type::TeamType;
+use vss_core_rust::domain::command::Command;
+use vss_core_rust::domain::wheels_command::WheelsCommand;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command_sender = CommandSender::new();
+    command_sender.create_socket(TeamType::Yellow);
+
+    let mut command = Command::new();
+
+    for i in 0..3 {
+        command.commands.push(WheelsCommand {
+            left_vel: 10.0,
+            right_vel: -10.0
+        });
+    }
+
+    loop {
+        thread::sleep(time::Duration::from_millis(10));
+        println!("{:?}", command);
+        command_sender.send_command(command.clone());
+    }
 }
