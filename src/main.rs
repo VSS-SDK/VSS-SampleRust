36834d7eba4df8fb22a7d008d78ff47fa289a327
John Fercher <johnathanfercher22@gmail.com>
2018-09-02 17:12:02 -0300

    atualizado exemplo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate vss_core_rust;
 
-use std::{thread, time};
-use self::vss_core_rust::communications::command_sender::CommandSender;
+use vss_core_rust::communications::command_sender::CommandSender;
 use vss_core_rust::domain::team_type::TeamType;
 use vss_core_rust::domain::command::Command;
 use vss_core_rust::domain::wheels_command::WheelsCommand;
+use vss_core_rust::communications::state_receiver::StateReceiver;
+use vss_core_rust::domain::field_transaformation_type::FieldTransformationType;
 
 fn main() {
     let mut command_sender = CommandSender::new();
+    let mut state_receiver = StateReceiver::new();
+
     command_sender.create_socket(TeamType::Yellow);
+    state_receiver.create_socket();
 
     let mut command = Command::new();
 
@@ -20,8 +24,7 @@ fn main() {
     }
 
     loop {
-        thread::sleep(time::Duration::from_millis(10));
-        println!("{:?}", command);
+        let state = state_receiver.receive_state(FieldTransformationType::None);
         command_sender.send_command(command.clone());
     }
 }
